838a9c5d293025fb6e9cf437fa6dfc6ccd64d98a
tekle.negash@mu.edu.et <TekEva>
2024-12-03 14:31:29 +0300

    repository name added to vite configruation for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from "dotenv";// import dotenv package for hiding environmental variables
-// run package config
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv'; // Import dotenv for environment variables
+
+// Load environment variables
 dotenv.config();
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  //define process env
+  base: '/Netflix-Clone-Tek-2024/', // Set to your repository name for deployment
+  build: {
+    outDir: 'dist', // Ensure output goes to 'dist'
+  },
 });
+
